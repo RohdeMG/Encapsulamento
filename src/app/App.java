b266a7133d36fbad59827ab2ecd68e8b1aebcd57
RohdeMG <rohde1392@gmail.com>
2020-02-18 16:18:04 +0000

    ok5

--- src/app/App.java
+++ src/app/App.java
@@ -17,6 +17,10 @@ desligar()).
 */ 
 
 
+
+//GIT
+//no git é só configurar o repositório, e email. O resto é tudo no vscode
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
