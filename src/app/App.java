310abb7d17aec1119bcdbcd9a009a4fdf0fb3a33
RohdeMG <rohde1392@gmail.com>
2020-02-18 16:09:06 +0000

    ok2

--- src/app/App.java
+++ src/app/App.java
@@ -1,5 +1,24 @@
 package app;
 
+//Encapsulamento (protege o código interno e vice versa)
+//metodo abstrato - método que não é desenvolvido na interface.
+/* ---------------------
+ <<interface>>									   
+ Controlador										   
+ =======================
+ ligar()
+ desligar()
+ volume+()
+ volume-()
+------------------------
+
+possui só os métodos(na interface são apresentadas
+só as ações. Ex: botões controle remoto; ligar(),
+desligar()).
+*/ 
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
