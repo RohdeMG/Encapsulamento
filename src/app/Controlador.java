ae62c4b49caec514c03cb189a392cb088659e50a
RohdeMG <rohde1392@gmail.com>
2020-02-21 11:10:09 +0000

    StArt

--- src/app/Controlador.java
+++ src/app/Controlador.java
@@ -3,6 +3,8 @@ package app;
 //Encapsulamento (protege o código interno e vice versa)
 //metodo abstrato - método que não é desenvolvido na interface.
 /* ---------------------
+
+
  <<interface>>									   
  Controlador										   
  =======================
@@ -11,9 +13,11 @@ package app;
  volume+()
  volume-()
 ------------------------
-possui só os métodos(na interface são apresentadas
-só as ações. Ex: botões controle remoto; ligar(),
+
+-possui só os métodos(na interface são apresentadas
+só as AÇÕES. Ex: botões controle remoto; ligar(),
 desligar()).
+-todos métodos dentro da interface são públicos(+).
 */ 
 
 
@@ -21,8 +25,8 @@ desligar()).
 //GIT
 //no git é só configurar o repositório, e email(cmd). O resto é tudo no vscode
 
-public class App {
+public class Controlador {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        
     }
 }
---
+++ src/app/ControleRemoto.java
@@ -0,0 +1,7 @@
+package app;
+
+public class ControleRemoto {
+    private boolean volume;
+    private boolean ligado;
+    private boolean tocando;
+}
